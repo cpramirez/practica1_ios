f22c4c6ac93f71991d3c81cee8a038d1555bc075
CICE <cice@aula03mac01.local>
2016-03-29 17:40:15 +0200

    Initial Commit

---
+++ APP_Test1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  APP_Test1
+//
+//  Created by CICE on 29/3/16.
+//  Copyright © 2016 CICE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
