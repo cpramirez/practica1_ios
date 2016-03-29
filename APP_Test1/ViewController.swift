25ee0802670b40e3f9ec0b7fb72fb5caa579e927
CICE <cice@aula03mac01.local>
2016-03-29 19:19:34 +0200

    No consigo hacer commit de todo

--- APP_Test1/ViewController.swift
+++ APP_Test1/ViewController.swift
@@ -9,12 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let labEs: String = "¡¡Hola mundo!!"
+    let labEn: String = "Hello world!!"
+    var language: Bool = false
+    
+    //MARK: - IBOUTLET
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myButtonClick: UIButton!
+    
+    //MARK - IBACTION
+    
+    @IBAction func myButtonAction(sender: AnyObject) {
+        if (language){
+            myLabel.text = labEs
+            language = false
+        } else {
+            myLabel.text = labEn
+            language = true
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Aplicacion cargada")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    //MARK: - CYCLE LIFE
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
